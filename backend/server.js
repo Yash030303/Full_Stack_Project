944d54b3d9674994b3aec090be94a70ae701c652
Yash030303 <110901151+Yash030303@users.noreply.github.com>
2026-04-06 21:47:39 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URI);
+
+const User = mongoose.model("User", {
+  name: String,
+  email: String,
+  age: Number
+});
+
+// API
+app.get("/api/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+// Add user (dynamic)
+app.post("/api/users", async (req, res) => {
+  try {
+    console.log("BODY:", req.body);
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//  Delete User
+app.delete("/api/users/:id", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.send("User Deleted");
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+app.listen(5000, "0.0.0.0", () => {
+  console.log("Server running");
+});
