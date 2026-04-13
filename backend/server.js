7ce726a6ec28f7e6b318e238a55f182e16dfd4b5
Yash030303 <110901151+Yash030303@users.noreply.github.com>
2026-04-13 20:33:10 +0530

    add CORS

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // ✅ MongoDB Connection + Start Server
 async function startServer() {
